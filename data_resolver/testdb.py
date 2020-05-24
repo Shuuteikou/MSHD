7edf5dc508da7d496353b2895ee5c7dbb7ec4c86
ziyao Xu <2460430476@qq.com>
2020-05-24 15:53:01 +0800

    write more data compare xutonglong

--- data_resolver/testdb.py
+++ data_resolver/testdb.py
@@ -52,6 +52,38 @@ class ModelTest(TestCase):
 		if d_statics.pk is not None:
 			d_statics.delete()
 		return
+        
+    def test_can_insert_in_DeathStatics_2(self):
+		d_statics = DeathStatics()
+		d_statics.id = '5118242010001131111'
+		d_statics.location = '四川省雅安市石棉县先锋藏族乡'
+		d_statics.date = '2020-05-03 18:06:12'
+		d_statics.number = '173'
+		d_statics.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if d_statics.pk is not None:
+			d_statics.delete()
+		self.assertIsNone(d_statics.pk)
+		d_statics.save()
+		self.assertIsNotNone(d_statics.pk)
+		if d_statics.pk is not None:
+			d_statics.delete()
+		return
+        
+    def test_can_insert_in_DeathStatics_3(self):
+		d_statics = DeathStatics()
+		d_statics.id = '1403111010001131111'
+		d_statics.location = '山西省阳泉市郊区河底镇 '
+		d_statics.date = '2020-05-13 08:06:12'
+		d_statics.number = '103'
+		d_statics.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if d_statics.pk is not None:
+			d_statics.delete()
+		self.assertIsNone(d_statics.pk)
+		d_statics.save()
+		self.assertIsNotNone(d_statics.pk)
+		if d_statics.pk is not None:
+			d_statics.delete()
+		return
 
 	#MissingStatics
 	def test_can_insert_in_MissingStatics(self):
@@ -69,8 +101,41 @@ class ModelTest(TestCase):
 		if m_statics.pk is not None:
 			m_statics.delete()
 		return
+    
+        
+    def test_can_insert_in_MissingStatics_2(self):
+		m_statics = MissingStatics()
+		m_statics.id = '5118242010001131101'
+		m_statics.location = '四川省雅安市石棉县先锋藏族乡'
+		m_statics.date = '2020-05-03 18:06:12'
+		m_statics.number = '17'
+		m_statics.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if m_statics.pk is not None:
+			m_statics.delete()
+		self.assertIsNone(m_statics.pk)
+		m_statics.save()
+		self.assertIsNotNone(m_statics.pk)
+		if m_statics.pk is not None:
+			m_statics.delete()
+		return    
 
-
+    def test_can_insert_in_MissingStatics_3(self):
+		m_statics = MissingStatics()
+		m_statics.id = '1403111010001131101'
+		m_statics.location = '山西省阳泉市郊区河底镇 '
+		m_statics.date = '2020-05-03 18:06:12'
+		m_statics.number = '17'
+		m_statics.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if m_statics.pk is not None:
+			m_statics.delete()
+		self.assertIsNone(m_statics.pk)
+		m_statics.save()
+		self.assertIsNotNone(m_statics.pk)
+		if m_statics.pk is not None:
+			m_statics.delete()
+		return
+        
+        
 	#CivilStructure
 	def test_can_insert_in_CivilStructure(self):
 		c_structure = CivilStructure()
@@ -90,12 +155,50 @@ class ModelTest(TestCase):
 		if c_structure.pk is not None:
 			c_structure.delete()
 		return
+        
+    def test_can_insert_in_CivilStructure_2(self):
+		c_structure = CivilStructure()
+		c_structure.id = '5118242010001120011'
+		c_structure.date = '2020-05-20 12:12:12'
+		c_structure.location = '四川省雅安市石棉县先锋藏族乡'
+		c_structure.basically_intact_square = '2500'
+		c_structure.damaged_square = '2000'
+		c_structure.destroyed_square = '1000'
+		c_structure.note = '破坏情况严重'
+		c_structure.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if c_structure.pk is not None:
+			c_structure.delete()
+		self.assertIsNone(c_structure.pk)
+		c_structure.save()
+		self.assertIsNotNone(c_structure.pk)
+		if c_structure.pk is not None:
+			c_structure.delete()
+		return
+        
+    def test_can_insert_in_CivilStructure_3(self):
+		c_structure = CivilStructure()
+		c_structure.id = '1403111010001120011'
+		c_structure.date = '2020-05-03 18:06:12'
+		c_structure.location = '山西省阳泉市郊区河底镇'
+		c_structure.basically_intact_square = '5000'
+		c_structure.damaged_square = '2500'
+		c_structure.destroyed_square = '1500'
+		c_structure.note = '破坏情况严重'
+		c_structure.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if c_structure.pk is not None:
+			c_structure.delete()
+		self.assertIsNone(c_structure.pk)
+		c_structure.save()
+		self.assertIsNotNone(c_structure.pk)
+		if c_structure.pk is not None:
+			c_structure.delete()
+		return
 
 	#MasonryStructure
 	def test_can_insert_in_MasonryStructure(self):
 		m_structure = MasonryStructure()
 		m_structure.id = '0101010020042231101'
-		m_structure.date = '2020-05-20 12:12:12'
+		m_structure.date = '2020-05-03 18:06:12'
 		m_structure.location = '江苏省南京市玄武区新街口街道大石桥社区某一区域'
 		m_structure.basically_intact_square = '3000'
 		m_structure.slight_damaged_square = '300'
@@ -112,6 +215,49 @@ class ModelTest(TestCase):
 		if m_structure.pk is not None:
 			m_structure.delete()
 		return 
+        
+    def test_can_insert_in_MasonryStructure_2(self):
+		m_structure = MasonryStructure()
+		m_structure.id = '511824201000111100'
+		m_structure.date = '2020-05-20 12:12:12'
+		m_structure.location = '四川省雅安市石棉县先锋藏族乡'
+		m_structure.basically_intact_square = '3000',
+		m_structure.slight_damaged_square = '1000'
+		m_structure.moderate_damaged_square = '500'
+		m_structure.serious_damaged_square = '300'
+		m_structure.destroyed_square = '400'
+		m_structure.note = '破坏情况严重'
+		m_structure.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if m_structure.pk is not None:
+			m_structure.delete()
+		self.assertIsNone(m_structure.pk)
+		m_structure.save()
+		self.assertIsNotNone(m_structure.pk)
+		if m_structure.pk is not None:
+			m_structure.delete()
+		return
+
+
+     def test_can_insert_in_MasonryStructure_3(self):
+		m_structure = MasonryStructure()
+		m_structure.id = '140311101000111100'
+		m_structure.date = '2020-05-13 08:06:12'
+		m_structure.location = '山西省阳泉市郊区河底镇'
+		m_structure.basically_intact_square = '2000',
+		m_structure.slight_damaged_square = '500'
+		m_structure.moderate_damaged_square = '300'
+		m_structure.serious_damaged_square = '200'
+		m_structure.destroyed_square = '500'
+		m_structure.note = '情况比较严重'
+		m_structure.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if m_structure.pk is not None:
+			m_structure.delete()
+		self.assertIsNone(m_structure.pk)
+		m_structure.save()
+		self.assertIsNotNone(m_structure.pk)
+		if m_structure.pk is not None:
+			m_structure.delete()
+		return
 
 	#CommDisaster
 	def test_can_insert_in_CommDisaster(self):
@@ -133,6 +279,47 @@ class ModelTest(TestCase):
 		if c_disaster.pk is not None:
 			c_disaster.delete()
 		return
+    
+    def test_can_insert_in_CommDisaster_2(self):
+		c_disaster = CommDisaster()
+		c_disaster.id = '5118242010002231110'
+		c_disaster.date = '2020-05-20 12:12:12'
+		c_disaster.location = '四川省雅安市石棉县先锋藏族乡'
+		c_disaster.type = '2'
+		c_disaster.data = '23.132,102.207'
+		c_disaster.structure = '2'
+		c_disaster.square = '150'
+		c_disaster.note = '严重'
+		c_disaster.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if c_disaster.pk is not None:
+			c_disaster.delete()
+		self.assertIsNone(c_disaster.pk)
+		c_disaster.save()
+		self.assertIsNotNone(c_disaster.pk)
+		if c_disaster.pk is not None:
+			c_disaster.delete()
+		return
+        
+    def test_can_insert_in_CommDisaster_3(self):
+		c_disaster = CommDisaster()
+		c_disaster.id = '1403111010002231110'
+		c_disaster.date = '2020-05-03 18:06:12'
+		c_disaster.location = '山西省阳泉市郊区河底镇'
+		c_disaster.type = '1'
+		c_disaster.data = '23.56,102.207'
+		c_disaster.structure = '2'
+		c_disaster.square = '200'
+		c_disaster.note = '严重'
+		c_disaster.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if c_disaster.pk is not None:
+			c_disaster.delete()
+		self.assertIsNone(c_disaster.pk)
+		c_disaster.save()
+		self.assertIsNotNone(c_disaster.pk)
+		if c_disaster.pk is not None:
+			c_disaster.delete()
+		return
+        
 
 
 	#TrafficDisaster
@@ -154,6 +341,44 @@ class ModelTest(TestCase):
 		if t_disaster.pk is not None:
 			t_disaster.delete()
 		return
+        
+    def test_can_insert_in_TrafficDisaster_2(self):
+		t_disaster = TrafficDisaster()
+		t_disaster.id = '5118242010001132022'
+		t_disaster.date = '2020-05-20 12:12:12'
+		t_disaster.location = '四川省雅安市石棉县先锋藏族乡'
+		t_disaster.type = '2'
+		t_disaster.grade = '1'
+		# t_disaster.picture = NULL 
+		t_disaster.note = '严重'
+		t_disaster.reporting_unit = '2201' + '四川省人民政府办公厅'
+		f t_disaster.pk is not None:
+			t_disaster.delete()
+		self.assertIsNone(t_disaster.pk)
+		t_disaster.save()
+		self.assertIsNotNone(t_disaster.pk)
+		if t_disaster.pk is not None:
+			t_disaster.delete()
+		return
+        
+    def test_can_insert_in_TrafficDisaster_3(self):
+		t_disaster = TrafficDisaster()
+		t_disaster.id = '1403111010001132022'
+		t_disaster.date = '2020-05-03 18:06:12'
+		t_disaster.location = '山西省阳泉市郊区河底镇'
+		t_disaster.type = '1'
+		t_disaster.grade = '1'
+		# t_disaster.picture = NULL 
+		t_disaster.note = '严重'
+		t_disaster.reporting_unit = '1103' + '山西省人民政府办公厅'
+		f t_disaster.pk is not None:
+			t_disaster.delete()
+		self.assertIsNone(t_disaster.pk)
+		t_disaster.save()
+		self.assertIsNotNone(t_disaster.pk)
+		if t_disaster.pk is not None:
+			t_disaster.delete()
+		return    
 
 
 	#CollapseRecord
@@ -167,6 +392,44 @@ class ModelTest(TestCase):
 		c_record.note = '较为严重'
 		# c_record.picture = NULL
 		c_record.reporting_unit = '2202' + '江苏省人民政府办公厅'
+		if c_record.pk is not None:
+			c_record.delete()
+		self.assertIsNone(c_record.pk)
+		c_record.save()
+		self.assertIsNotNone(c_record.pk)
+		if c_record.pk is not None:
+			c_record.delete()
+		return
+        
+    def test_can_insert_in_CollapseRecord_2(self):
+		c_record = CollapseRecord()
+		c_record.id = '5118242010001132021'
+		c_record.location = '四川省雅安市石棉县先锋藏族乡'
+		c_record.date = '2020-05-20 12:12:12'
+		c_record.type = '1'
+		c_record.status = '1'
+		c_record.note = '严重'
+		# c_record.picture = NULL
+		c_record.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if c_record.pk is not None:
+			c_record.delete()
+		self.assertIsNone(c_record.pk)
+		c_record.save()
+		self.assertIsNotNone(c_record.pk)
+		if c_record.pk is not None:
+			c_record.delete()
+		return
+
+    def test_can_insert_in_CollapseRecord_3(self):
+		c_record = CollapseRecord()
+		c_record.id = '5118242010001132021'
+		c_record.location = '山西省阳泉市郊区河底镇'
+		c_record.date = '2020-05-03 18:06:12'
+		c_record.type = '1'
+		c_record.status = '2'
+		c_record.note = '严重'
+		# c_record.picture = NULL
+		c_record.reporting_unit = '1103' + '山西省人民政府办公厅'
 		if c_record.pk is not None:
 			c_record.delete()
 		self.assertIsNone(c_record.pk)
@@ -195,6 +458,45 @@ class ModelTest(TestCase):
 		if l_record.pk is not None:
 			l_record.delete()
 		return
+        
+    def test_can_insert_in_LandslideRecord_2(self):
+		l_record = LandslideRecord()
+		l_record.id = '5118242010001132020'
+		l_record.location = '四川省雅安市石棉县先锋藏族乡'
+		l_record.date = '2020-05-20 12:12:12'
+		l_record.type = '2'
+		l_record.status = '2'
+		l_record.note = '较为严重'
+		# l_record.picture = NULL
+		l_record.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if l_record.pk is not None:
+			l_record.delete()
+		self.assertIsNone(l_record.pk)
+		l_record.save()
+		self.assertIsNotNone(l_record.pk)
+		if l_record.pk is not None:
+			l_record.delete()
+		return
+        
+    def test_can_insert_in_LandslideRecord_3(self):
+		l_record = LandslideRecord()
+		l_record.id = '1403111010001132020'
+		l_record.location = '山西省阳泉市郊区河底镇'
+		l_record.date = '2020-05-03 18:06:12'
+		l_record.type = '1'
+		l_record.status = '2'
+		l_record.note = '较为严重'
+		# l_record.picture = NULL
+		l_record.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if l_record.pk is not None:
+			l_record.delete()
+		self.assertIsNone(l_record.pk)
+		l_record.save()
+		self.assertIsNotNone(l_record.pk)
+		if l_record.pk is not None:
+			l_record.delete()
+		return
+    
 
 	#DisasterInfo
 	def test_can_insert_in_DisasterInfo(self):
@@ -216,6 +518,46 @@ class ModelTest(TestCase):
 		if d_info.pk is not None:
 			d_info.delete()
 		return
+        
+    def test_can_insert_in_DisasterInfo_2(self):
+		d_info = DisasterInfo()
+		d_info.id = '5118242010001131220'
+		d_info.date = '2020-05-20 12:12:12'
+		d_info.location = '四川省雅安市石棉县先锋藏族乡'
+		d_info.longtitude = '102.281929'
+		d_info.latitude = '29.280413'
+		d_info.depth = '43.45'
+		d_info.magnitude = '7'
+		# d_info.picture = NULL
+		d_info.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if d_infp.pk is not None:
+			d_info.delete()
+		self.assertIsNone(d_info.pk)
+		d_info.save()
+		self.assertIsNotNone(d_info.pk)
+		if d_info.pk is not None:
+			d_info.delete()
+		return
+        
+    def test_can_insert_in_DisasterInfo_3(self):
+		d_info = DisasterInfo()
+		d_info.id = '1403111010001131220'
+		d_info.date = '2020-05-13 08:06:12'
+		d_info.location = '山西省阳泉市郊区河底镇'
+		d_info.longtitude = '113.57'
+		d_info.latitude = '38.00'
+		d_info.depth = '56.12'
+		d_info.magnitude = '7'
+		# d_info.picture = NULL
+		d_info.reporting_unit = '1103' + '山西省人民政府办公厅'
+		if d_infp.pk is not None:
+			d_info.delete()
+		self.assertIsNone(d_info.pk)
+		d_info.save()
+		self.assertIsNotNone(d_info.pk)
+		if d_info.pk is not None:
+			d_info.delete()
+		return    
 
 
 	#DisasterPrediction
@@ -233,6 +575,52 @@ class ModelTest(TestCase):
 		d_prediction.note = '受灾严重'
 		# d_prediction.picture = '0000'
 		d_prediction.reporting_unit = '2202' + '江苏省人民政府办公厅'
+		if d_prediction.pk is not None:
+			d_prediction.delete()
+		self.assertIsNone(d_prediction.pk)
+		d_prediction.save()
+		self.assertIsNotNone(d_prediction.pk)
+		if d_prediction.pk is not None:
+			d_prediction.delete()
+		return
+        
+    def test_can_insert_DisasterPrediction_2(self):
+		d_prediction = DisatserPrediction()
+		d_prediction.id = '5118242010001131221'
+		d_prediction.date = '2020-05-20 12:12:12'
+		d_prediction.location = '四川省雅安市石棉县先锋藏族乡'
+		d_prediction.longtitude = '102.281929'
+		d_prediction.latitude = '29.280413'
+		d_prediction.depth = '43.45'
+		d_prediction.magnitude = '5.4'
+		d_prediction.intensity = '7'
+		d_prediction.type = '1'
+		d_prediction.note = '受灾严重'
+		# d_prediction.picture = '0000'
+		d_prediction.reporting_unit = '2201' + '四川省人民政府办公厅'
+		if d_prediction.pk is not None:
+			d_prediction.delete()
+		self.assertIsNone(d_prediction.pk)
+		d_prediction.save()
+		self.assertIsNotNone(d_prediction.pk)
+		if d_prediction.pk is not None:
+			d_prediction.delete()
+		return
+    
+    def test_can_insert_DisasterPrediction_3(self):
+		d_prediction = DisatserPrediction()
+		d_prediction.id = '1403111010001131221'
+		d_prediction.date = '2020-05-03 18:06:12'
+		d_prediction.location = '山西省阳泉市郊区河底镇'
+		d_prediction.longtitude = '113.57'
+		d_prediction.latitude = '38.00'
+		d_prediction.depth = '56.12'
+		d_prediction.magnitude = '5.8'
+		d_prediction.intensity = '7'
+		d_prediction.type = '1'
+		d_prediction.note = '受灾严重'
+		# d_prediction.picture = '0000'
+		d_prediction.reporting_unit = '1103' + '山西省人民政府办公厅'
 		if d_prediction.pk is not None:
 			d_prediction.delete()
 		self.assertIsNone(d_prediction.pk)
@@ -259,7 +647,41 @@ class ModelTest(TestCase):
 		if d_request.pk is not None:
 			d_request.delete()
 		return
+        
+    def test_can_insert_in_DisasterRequest_2(self):
+		d_request = DisasterRequest()
+		d_request.id = '5118242010001131224'
+		d_request.date = '2020-05-18 17:36:12'
+		d_request.disasterType = d_request.id[12:15]
+		d_request.status = '1'
+		# d_request.o_URL = 'NULL'
+		d_request.requestunit = '四川省人民政府办公厅'
+		if d_request.pk is not None:
+			d_request.delete()
+		self.assertIsNone(d_request.pk)
+		d_request.save()
+		self.assertIsNotNone(d_request.pk)
+		if d_request.pk is not None:
+			d_request.delete()
+		return
 
+
+    def test_can_insert_in_DisasterRequest_3(self):
+		d_request = DisasterRequest()
+		d_request.id = '1403111010001131224'
+		d_request.date = '2020-05-03 18:06:12'
+		d_request.disasterType = d_request.id[12:15]
+		d_request.status = '1'
+		# d_request.o_URL = 'NULL'
+		d_request.requestunit = '山西省人民政府办公厅'
+		if d_request.pk is not None:
+			d_request.delete()
+		self.assertIsNone(d_request.pk)
+		d_request.save()
+		self.assertIsNotNone(d_request.pk)
+		if d_request.pk is not None:
+			d_request.delete()
+		return
 
 
 
