671470f111507ffb57c728c6bc27e08dc234568a
dimpurr <dimpurr@live.com>
2020-06-02 17:56:14 +0800

    insert

--- data_resolver/urls.py
+++ data_resolver/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('index_20200504',views.index_20200504,name='index_20200504'),
     path('index_20200514',views.index_20200514,name='index_20200514'),
     path('index_20200519',views.index_20200519,name='index_20200519'),
-    path('insert',views.insert_DeathStatics,name="insert_DeathStatics"),
+    path('insert_DeathStatics',views.insert_DeathStatics,name="insert_DeathStatics"),
     path('details_xmxx',views.details_xmxx,name='details_xmxx'),
     path('index',views.index,name='index'),
 
