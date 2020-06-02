6529e4f0d76815a203988ebbc26d645b46a0c4d3
Kirbyszsr <luobocun@qq.com>
2020-06-02 22:05:51 +0800

    iter5:实现多个数据库的连接

--- MSHD_project13/settings.py
+++ MSHD_project13/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'backup_db':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'backup_db.sqlite3')
     }
 }
 
